c1c0c13334eee7bce3ae96940c784b34feb99116
slowblitz <jeremygarcia937@gmail.com>
2020-04-24 14:52:59 +0200

    adding core code

--- checker/AnDOChecker.py
+++ checker/AnDOChecker.py
@@ -1,1 +1,110 @@
-#
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import errno
+import json
+import sys
+import re
+import argparse
+from AnDO_Error import (
+ 
+    SourceError, 
+    DateError, 
+    SubError)
+
+from AnDO_engine import *
+
+dir_rules = os.path.join(os.path.dirname(__file__)) + 'rules/'
+
+
+def path_hierarchy(path):
+    """[make a json tree structure of the folder given in parameter]
+    Arguments:
+        path {[type]} -- [Path to the folder]
+    Returns:
+        [json] -- [Tree of the folder]
+    """
+
+    hierarchy = {'type': 'directory', 'name': os.path.basename(path),
+                 'path': path}
+
+    try:
+        hierarchy['children'] = [path_hierarchy(os.path.join(path,
+                                 contents)) for contents in
+                                 os.listdir(path)]
+    except OSError as e:
+        if e.errno != errno.ENOTDIR:
+            raise
+        hierarchy['type'] = 'file'
+
+    return hierarchy
+
+
+def get_name_in_dir(list_dict, names):
+    """[Get all the name of directories in the Dict recusively]
+    Arguments:
+        list_dict {[Dict]} -- [Json to dict ]
+        names {[list]} -- [Names of directories founds]
+    Returns:
+        [list] -- [Names of directories founds]
+    """
+
+    for my_dict in list_dict:
+
+        if my_dict['type'] == 'directory':
+            names.append(my_dict['name'])
+            names = get_name_in_dir(my_dict['children'], names)
+
+    return names
+
+
+if __name__ == '__main__':
+
+    # add argparse for verbose option
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='increase output verbosity')
+    parser.add_argument('path', help='Path to your folder ')
+    args = parser.parse_args()
+
+    if args.verbose:
+        try:
+            directory = args.path
+        except IndexError:
+
+            directory = '.'
+        names = []
+        dic_data = json.loads(json.dumps(path_hierarchy(directory),
+                              indent=2, sort_keys=True))
+        names = get_name_in_dir([dic_data], names)
+
+        error = is_bids_verbose(names)
+
+        if error == 1:
+            print ("\n" +
+                    directory
+                   +": Is Not validated by AnDOChecker")
+        else:
+            print ("\n" + directory 
+                  + ": Is validated by AnDOChecker")
+    else:
+        try:
+            directory = args.path
+        except IndexError:
+
+            directory = '.'
+
+        dic_data = json.loads(json.dumps(path_hierarchy(directory),
+                              indent=2, sort_keys=True))
+
+        names = []
+        names = get_name_in_dir([dic_data], names)
+        error_not_found = is_bids(names)
+        if not error_not_found:
+            print ("\n" + directory 
+                   +": Is Not validated by AnDOChecker")
+        else:
+            print ("\n" + directory 
+                   +": Is validated by AnDOChecker")
---
+++ checker/AnDO_engine.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import errno
+import json
+import sys
+import re
+from AnDO_Error import (
+    SourceError,
+    DateError,
+    SubError)
+
+dir_rules = os.path.join(os.path.dirname(__file__)) + '/rules/'
+
+
+def is_bids(names):
+    """Checks if a file path appropriate for BIDS.
+
+        Main method of the validator. uses other class methods for checking
+        different aspects of the file path.
+
+        Parameters
+        ----------
+            path: string
+                A path of a file you want to check.
+        """
+
+    validate = []
+    validate.append(is_date(names))
+    validate.append(is_source(names))
+    validate.append(is_sub(names))
+
+    return all(validate)
+
+
+def is_bids_verbose(names):
+    bool_error = 0
+    if is_date(names):
+        bool_error = 0
+    else:
+        try:
+            raise DateError(names)
+        except DateError as e:
+            print (e.strerror)
+            bool_error = 1
+    if is_sub(names):
+        bool_error = 0
+    else:
+        try:
+            raise SubError(names)
+        except SubError as e:
+            print (e.strerror)
+            bool_error = 1
+    if is_source(names):
+        bool_error = 0
+    else:
+        try:
+            raise SourceError(names)
+        except SourceError as e:
+            print (e.strerror)
+            bool_error = 1
+
+    return bool_error
+
+
+def is_date(names):
+    ''' Check if file is data. '''
+
+    regexps = get_regular_expressions(dir_rules + 'date_rules.json')
+    conditions = []
+    for word in names:
+        conditions.append([re.compile(x).search(word) is not None
+                          for x in regexps])
+
+        # print(flatten(conditions))
+
+    return any(flatten(conditions))
+
+
+def is_data(names):
+    ''' Check if file is data. '''
+
+    regexps = get_regular_expressions(dir_rules + 'data_rules.json')
+    conditions = []
+    for word in names:
+        conditions.append([re.compile(x).match(word) is not None
+                          for x in regexps])
+
+        # print(flatten(conditions))
+
+    return any(flatten(conditions))
+
+
+def is_sub(names):
+    ''' Check if file is data. '''
+
+    regexps = get_regular_expressions(dir_rules + 'subject_rules.json')
+    conditions = []
+    for word in names:
+        conditions.append([re.compile(x).search(word) is not None
+                          for x in regexps])
+
+        #  print(flatten(conditions))
+
+    return any(flatten(conditions))
+
+
+def is_source(names):
+    ''' Check if file is data. '''
+
+    regexps = get_regular_expressions(dir_rules + 'source_rules.json')
+    conditions = []
+    for word in names:
+        conditions.append([re.compile(x).search(word) is not None
+                          for x in regexps])
+
+        #  print(flatten(conditions))
+
+    return any(flatten(conditions))
+
+
+def get_regular_expressions(fileName):
+    '''
+    https://github.com/bids-standard/bids-validator/tree/master/bids-validator/
+
+    using function to read regex in rule files
+
+    '''
+
+    regexps = []
+
+    with open(fileName, 'r') as f:
+        rules = json.load(f)
+
+    for key in list(rules.keys()):
+        rule = rules[key]
+
+        regexp = rule['regexp']
+
+        if 'tokens' in rule:
+            tokens = rule['tokens']
+
+            for token in list(tokens):
+                regexp = regexp.replace(token, '|'.join(tokens[token]))
+
+        regexps.append(regexp)
+
+    return regexps
+
+
+def flatten(seq):
+    l = []
+    for elt in seq:
+        t = type(elt)
+        if t is tuple or t is list:
+            for elt2 in flatten(elt):
+                l.append(elt2)
+        else:
+            l.append(elt)
+    return l
