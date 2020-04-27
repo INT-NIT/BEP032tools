eae76b9fa0dae802a9ad5dbcfaa806f8cb4b85e2
Slowblitz <jeremygarcia937@gmail.com>
2020-04-27 11:51:32 +0200

    CoreCode pep8 ready (#3)
    * Adding travis file (dont forget to enable it )
    
    * Update LICENSE
    
    * Update .travis.yml
    
    * adding core code
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * adding "ls" in script to pass building
    
    * flake8 update
    
    * Replacing date with session
    
    * fixing docstring and comment
    
    * docstring and Session fix
    
    * fixing bug
    
    * Deleting bids references
    
    * Delete AnDO_Error.cpython-35.pyc
    
    * Delete AnDO_engine.cpython-35.pyc
    
    Co-authored-by: David Meunier <david_meunier_79@hotmail.fr>

--- checker/AnDOChecker.py
+++ checker/AnDOChecker.py
@@ -1,1 +1,118 @@
-#
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import errno
+import json
+
+import argparse
+
+from AnDO_engine import is_AnDO, is_AnDO_verbose
+
+dir_rules = os.path.join(os.path.dirname(__file__)) + 'rules/'
+
+
+def path_hierarchy(path):
+    """
+    Make a json tree structure of the folder given in parameter]
+    
+    :param path: Path to the folder 
+    """
+
+    hierarchy = {'type': 'directory', 'name': os.path.basename(path),
+                 'path': path}
+
+    try:
+        hierarchy['children'] = [path_hierarchy(os.path.join(path,
+                                 contents)) for contents in
+                                 os.listdir(path)]
+    except OSError as e:
+        if e.errno != errno.ENOTDIR:
+            raise
+        hierarchy['type'] = 'file'
+
+    return hierarchy
+
+
+def get_name_in_dir(list_dict, names):
+    """
+    Get all the name of directories in the Dict recusively
+    
+    :param list_dict: Json to dict 
+    :param names: Names of directories founds
+    """
+
+    for my_dict in list_dict:
+
+        if my_dict['type'] == 'directory':
+            names.append(my_dict['name'])
+            names = get_name_in_dir(my_dict['children'], names)
+
+    return names
+
+
+if __name__ == '__main__':
+    """
+    Main function :
+    
+    usage: AnDOChecker.py [-h] [-v] pathToFolder
+
+        positional arguments:
+        path           Path to your folder
+
+        optional arguments:
+        -h, --help     show this help message and exit
+        -v, --verbose  increase output verbosity
+        
+    """
+    # add argparse for verbose option
+    
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='increase output verbosity')
+    parser.add_argument('path', help='Path to your folder ')
+    args = parser.parse_args()
+
+    if args.verbose:
+        try:
+            directory = args.path
+        except IndexError:
+
+            directory = '.'
+        names = []
+        dic_data = json.loads(json.dumps(path_hierarchy(directory),
+                              indent=2, sort_keys=True))
+        names = get_name_in_dir([dic_data], names)
+
+        error = is_AnDO_verbose(names)
+
+        if error == 1:
+            print("\n" +
+                  directory +
+                  ": Is Not validated by AnDOChecker")
+        else:
+            print("\n" +
+                  directory +
+                  ": Is validated by AnDOChecker")
+    else:
+        try:
+            directory = args.path
+        except IndexError:
+
+            directory = '.'
+
+        dic_data = json.loads(json.dumps(path_hierarchy(directory),
+                              indent=2, sort_keys=True))
+
+        names = []
+        names = get_name_in_dir([dic_data], names)
+        error_not_found = is_AnDO(names)
+        if not error_not_found:
+            print("\n" +
+                  directory +
+                  ": Is Not validated by AnDOChecker")
+        else:
+            print("\n" +
+                  directory +
+                  ": Is validated by AnDOChecker")
---
+++ checker/AnDO_engine.py
@@ -0,0 +1,168 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import json
+import re
+from AnDO_Error import (
+    SourceError,
+    SessionError,
+    SubError)
+
+dir_rules = os.path.join(os.path.dirname(__file__)) + '/rules/'
+
+
+def is_AnDO(names):
+    """
+    Check if file path adheres to AnDO.
+    Main method of the validator. uses other class methods for checking
+    different aspects of the directory path.
+
+    :param names: 
+    """
+    validate = []
+    validate.append(is_session(names))
+    validate.append(is_source(names))
+    validate.append(is_subject(names))
+
+    return all(validate)
+
+
+def is_AnDO_verbose(names):
+    """
+    Check if file path adheres to AnDO.
+    Main method of the validator. uses other class methods for checking
+    different aspects of the directory path.
+    
+    :param names: list of names founds in the path
+    """
+    bool_error = 0
+    if is_session(names):
+        bool_error = 0
+    else:
+        try:
+            raise SessionError(names)
+        except SessionError as e:
+            print(e.strerror)
+            bool_error = 1
+    if is_subject(names):
+        bool_error = 0
+    else:
+        try:
+            raise SubError(names)
+        except SubError as e:
+            print(e.strerror)
+            bool_error = 1
+    if is_source(names):
+        bool_error = 0
+    else:
+        try:
+            raise SourceError(names)
+        except SourceError as e:
+            print(e.strerror)
+            bool_error = 1
+
+    return bool_error
+
+
+def is_session(names):
+    """
+    Check names follows session rules 
+    
+    :param names: list of names founds in the path
+    """
+
+    regexps = get_regular_expressions(dir_rules + 'session_rules.json')
+    conditions = []
+    for word in names:
+        conditions.append([re.compile(x).search(word) is not None
+                          for x in regexps])
+
+        # print(flatten(conditions))
+
+    return any(flatten(conditions))
+
+
+
+def is_subject(names):
+    """
+    Check names follows subject rules
+    
+    :param names: list of names founds in the path
+    """
+
+    regexps = get_regular_expressions(dir_rules + 'subject_rules.json')
+    conditions = []
+    for word in names:
+        conditions.append([re.compile(x).search(word) is not None
+                          for x in regexps])
+
+        #  print(flatten(conditions))
+
+    return any(flatten(conditions))
+
+
+def is_source(names):
+    """
+    Check names follows source rules
+    
+    :param names: list of names founds in the path
+    """
+
+    regexps = get_regular_expressions(dir_rules + 'source_rules.json')
+    conditions = []
+    for word in names:
+        conditions.append([re.compile(x).search(word) is not None
+                          for x in regexps])
+
+        #  print(flatten(conditions))
+
+    return any(flatten(conditions))
+
+
+def get_regular_expressions(fileName):
+    '''
+    https://github.com/bids-standard/bids-validator/tree/master/bids-validator/
+
+    using function to read regex in rule files
+
+    '''
+
+    regexps = []
+
+    with open(fileName, 'r') as f:
+        rules = json.load(f)
+
+    for key in list(rules.keys()):
+        rule = rules[key]
+
+        regexp = rule['regexp']
+
+        if 'tokens' in rule:
+            tokens = rule['tokens']
+
+            for token in list(tokens):
+                regexp = regexp.replace(token, '|'.join(tokens[token]))
+
+        regexps.append(regexp)
+
+    return regexps
+
+
+def flatten(seq):
+    """
+    Format list the proper way
+    exemple:
+    [[x],[y],[z]]--->[x,y,z]
+    
+    :param seq: list to format
+    """
+    list_flaten = []
+    for elt in seq:
+        t = type(elt)
+        if t is tuple or t is list:
+            for elt2 in flatten(elt):
+                list_flaten.append(elt2)
+        else:
+            list_flaten.append(elt)
+    return list_flaten
