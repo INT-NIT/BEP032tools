1af17c4066b8bfd37cd3dcce775f23ff024824ab
slowblitz <jeremygarcia937@gmail.com>
2020-04-27 19:10:03 +0200

    fix "ANDO" and "PATH" replacing by lower case

--- checker/tests/test_isAnDO.py
+++ checker/tests/test_isAnDO.py
@@ -1,8 +1,8 @@
 import unittest
-import AnDOChecker as ANDO
+import AnDOChecker as ando
 import json
 import os
-PATH=os.getcwd()
+path=os.getcwd()
 
 
 class test_AnDO(unittest.TestCase):
@@ -15,43 +15,43 @@ class test_AnDO(unittest.TestCase):
     '''testing ds001 folder'''
     def test_bids_dataset_1(self):
         names=list()
-        test1=PATH+"/tests/ds001/Data/Landing"
+        test1=path+"/tests/ds001/Data/Landing"
         dic_data = json.loads(
-        json.dumps(ANDO.path_hierarchy(test1), indent=2, sort_keys=True))
-        names = ANDO.get_name_in_dir([dic_data], names)
-        self.assertEqual(ANDO.is_AnDO(names), False)
+        json.dumps(ando.path_hierarchy(test1), indent=2, sort_keys=True))
+        names = ando.get_name_in_dir([dic_data], names)
+        self.assertEqual(ando.is_AnDO(names), False)
     '''testing ds002 folder'''
     def test_bids_dataset_2(self):
         names=list()
-        test1=PATH+"/tests/ds002/data/Landing"
+        test1=path+"/tests/ds002/data/Landing"
         dic_data = json.loads(
-        json.dumps(ANDO.path_hierarchy(test1), indent=2, sort_keys=True))
-        names = ANDO.get_name_in_dir([dic_data], names)
-        self.assertEqual(ANDO.is_AnDO(names), True)
+        json.dumps(ando.path_hierarchy(test1), indent=2, sort_keys=True))
+        names = ando.get_name_in_dir([dic_data], names)
+        self.assertEqual(ando.is_AnDO(names), True)
     '''testing ds003 folder'''
     def test_bids_dataset_3(self):
         names=list()
-        test1=PATH+"/tests/ds003/data/my_experiment"
+        test1=path+"/tests/ds003/data/my_experiment"
         dic_data = json.loads(
-        json.dumps(ANDO.path_hierarchy(test1), indent=2, sort_keys=True))
-        names = ANDO.get_name_in_dir([dic_data], names)
-        self.assertEqual(ANDO.is_AnDO(names), False)
+        json.dumps(ando.path_hierarchy(test1), indent=2, sort_keys=True))
+        names = ando.get_name_in_dir([dic_data], names)
+        self.assertEqual(ando.is_AnDO(names), False)
     '''testing ds004 folder'''
     def test_bids_dataset_4(self):
         names=list()
-        test1=PATH+"/tests/ds004/data/newexp_vision"
+        test1=path+"/tests/ds004/data/newexp_vision"
         dic_data = json.loads(
-        json.dumps(ANDO.path_hierarchy(test1), indent=2, sort_keys=True))
-        names = ANDO.get_name_in_dir([dic_data], names)
-        self.assertEqual(ANDO.is_AnDO(names), False)
+        json.dumps(ando.path_hierarchy(test1), indent=2, sort_keys=True))
+        names = ando.get_name_in_dir([dic_data], names)
+        self.assertEqual(ando.is_AnDO(names), False)
     '''testing ds005 folder'''
     def test_bids_dataset_5(self):
         names=list()
-        test1=PATH+"/tests/ds005/data/my_experiment"
+        test1=path+"/tests/ds005/data/my_experiment"
         dic_data = json.loads(
-        json.dumps(ANDO.path_hierarchy(test1), indent=2, sort_keys=True))
-        names = ANDO.get_name_in_dir([dic_data], names)
-        self.assertEqual(ANDO.is_AnDO(names), False)
+        json.dumps(ando.path_hierarchy(test1), indent=2, sort_keys=True))
+        names = ando.get_name_in_dir([dic_data], names)
+        self.assertEqual(ando.is_AnDO(names), False)
     
 
 if __name__ == '__main__':
