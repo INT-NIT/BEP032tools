2d82add78a78908f3e2e2805931ad8b5dc73bae3
Slowblitz <jeremygarcia937@gmail.com>
2020-04-27 19:28:23 +0200

    Adding unitTest and update readme (#7)
    * adding datasets
    
    * UnitTest on rules and dataset
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Add files via upload
    
    * Update README.md
    
    * modifiying README
    
    * fix "ANDO" and "PATH" replacing by lower case
    
    * replacing "ANDO" by "ando

---
+++ checker/tests/test_Rules.py
@@ -0,0 +1,43 @@
+import unittest
+import AnDO_engine as ando
+class Test_Rules(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    '''testing session rules'''
+    def test_session_rules(self):
+        list=["180116_001_m_enya_land-001"]
+        self.assertEqual(ando.is_session(list), True)
+    def test_session_rules_1(self):
+        list=["180116_m_enya_land-001"]
+        self.assertEqual(ando.is_session(list), False)
+    def test_session_rules_2(self):
+        list=["180116_001_m_land-001"]
+        self.assertEqual(ando.is_session(list), False)
+
+
+    '''testing source rules'''
+    def test_source_rules(self):
+        list=["source"]
+        self.assertEqual(ando.is_source(list), True)
+    def test_source_rules_1(self):
+        list=["Source"]
+        self.assertEqual(ando.is_source(list), False)
+    def test_source_rules_2(self):
+        list=["sources"]
+        self.assertEqual(ando.is_source(list), False)
+
+    '''testing subject rules'''
+    def test_subject_rules(self):
+        list=["sub-001"]
+        self.assertEqual(ando.is_subject(list), True)
+    def test_subject_rules_1(self):
+        list=["sub_001"]
+        self.assertEqual(ando.is_subject(list), False)
+    def test_subject_rules_2(self):
+        list=["Sub_001"]
+        self.assertEqual(ando.is_subject(list), False)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ checker/tests/test_isAnDO.py
@@ -0,0 +1,59 @@
+import unittest
+import AnDOChecker as ando
+import json
+import os
+path=os.getcwd()
+
+
+class test_AnDO(unittest.TestCase):
+
+    
+    def setUp(self):
+        pass
+    
+    
+    '''testing ds001 folder'''
+    def test_bids_dataset_1(self):
+        names=list()
+        test1=path+"/tests/ds001/Data/Landing"
+        dic_data = json.loads(
+        json.dumps(ando.path_hierarchy(test1), indent=2, sort_keys=True))
+        names = ando.get_name_in_dir([dic_data], names)
+        self.assertEqual(ando.is_AnDO(names), False)
+    '''testing ds002 folder'''
+    def test_bids_dataset_2(self):
+        names=list()
+        test1=path+"/tests/ds002/data/Landing"
+        dic_data = json.loads(
+        json.dumps(ando.path_hierarchy(test1), indent=2, sort_keys=True))
+        names = ando.get_name_in_dir([dic_data], names)
+        self.assertEqual(ando.is_AnDO(names), True)
+    '''testing ds003 folder'''
+    def test_bids_dataset_3(self):
+        names=list()
+        test1=path+"/tests/ds003/data/my_experiment"
+        dic_data = json.loads(
+        json.dumps(ando.path_hierarchy(test1), indent=2, sort_keys=True))
+        names = ando.get_name_in_dir([dic_data], names)
+        self.assertEqual(ando.is_AnDO(names), False)
+    '''testing ds004 folder'''
+    def test_bids_dataset_4(self):
+        names=list()
+        test1=path+"/tests/ds004/data/newexp_vision"
+        dic_data = json.loads(
+        json.dumps(ando.path_hierarchy(test1), indent=2, sort_keys=True))
+        names = ando.get_name_in_dir([dic_data], names)
+        self.assertEqual(ando.is_AnDO(names), False)
+    '''testing ds005 folder'''
+    def test_bids_dataset_5(self):
+        names=list()
+        test1=path+"/tests/ds005/data/my_experiment"
+        dic_data = json.loads(
+        json.dumps(ando.path_hierarchy(test1), indent=2, sort_keys=True))
+        names = ando.get_name_in_dir([dic_data], names)
+        self.assertEqual(ando.is_AnDO(names), False)
+    
+
+if __name__ == '__main__':
+    unittest.main()
+ 
