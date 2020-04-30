e1f39a7280a38cb117530bf7febe714a578d0d93
slowblitz <jeremygarcia937@gmail.com>
2020-04-30 13:34:06 +0200

    new main

--- checker/AnDOChecker.py
+++ checker/AnDOChecker.py
@@ -11,45 +11,6 @@ from AnDO_engine import is_AnDO, is_AnDO_verbose
 dir_rules = os.path.join(os.path.dirname(__file__)) + 'rules/'
 
 
-def path_hierarchy(path):
-    """
-    Make a json tree structure of the folder given in parameter]
-    
-    :param path: Path to the folder 
-    """
-
-    hierarchy = {'type': 'directory', 'name': os.path.basename(path),
-                 'path': path}
-
-    try:
-        hierarchy['children'] = [path_hierarchy(os.path.join(path,
-                                 contents)) for contents in
-                                 os.listdir(path)]
-    except OSError as e:
-        if e.errno != errno.ENOTDIR:
-            raise
-        hierarchy['type'] = 'file'
-
-    return hierarchy
-
-
-def get_name_in_dir(list_dict, names):
-    """
-    Get all the name of directories in the Dict recusively
-    
-    :param list_dict: Json to dict 
-    :param names: Names of directories founds
-    """
-
-    for my_dict in list_dict:
-
-        if my_dict['type'] == 'directory':
-            names.append(my_dict['name'])
-            names = get_name_in_dir(my_dict['children'], names)
-
-    return names
-
-
 
 if __name__ == '__main__':
     """
@@ -80,12 +41,9 @@ if __name__ == '__main__':
         except IndexError:
 
             directory = '.'
-        names = []
-        dic_data = json.loads(json.dumps(path_hierarchy(directory),
-                              indent=2, sort_keys=True))
-        names = get_name_in_dir([dic_data], names)
+       
 
-        error = is_AnDO_verbose(names)
+        error = is_AnDO_verbose(directory)
 
         if error == 1:
             print("\n" +
@@ -101,10 +59,6 @@ if __name__ == '__main__':
         except IndexError:
 
             directory = '.'
-
-        dic_data = json.loads(json.dumps(path_hierarchy(directory),
-                              indent=2, sort_keys=True))
-        
     
     
         error_not_found = is_AnDO(directory)
