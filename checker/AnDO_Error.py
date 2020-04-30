d504247e1c99e00389d0e256a7c77bc5d661f2b7
slowblitz <jeremygarcia937@gmail.com>
2020-04-30 16:18:02 +0200

    More lisibility

--- checker/AnDO_Error.py
+++ checker/AnDO_Error.py
@@ -34,27 +34,21 @@ class SessionError(Exception):
             + names[2] + '\n' \
             + '\nIt does not respect the naming convention for a session directory,which is (yymmdd_xxx_a_bbbb_cccc,) with : ' \
             + '\n' '''
-
             ''' \
             + '- yymmdd is the date in the format year, month, date (6 digits, for instance 200430 for April 30, 2020)' \
             + '''
-
             ''' \
             + '- xxx is the number of the session acquired on that date (3 digits, for instance 001 for the first session)' \
             + '''
-
             ''' \
             + '- a is a single letter that designate the species (m for ???, o for ???, r for ???, s for ???)' \
             + '''
-
             ''' \
             + '- bbbb is a string containing the user friendly identifier (UFID) of the animal' \
             + '''
-
             ''' \
             + '- cccc is a string of which the use is to be decided by the research group / user ' \
             + '''
-
             ''' \
             + '(for instance to add extra info on the version of the experimental protocol, on the type of preparation etc.)' \
             + '\n'
