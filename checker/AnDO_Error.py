ea48524596c11742d61452055ad2deef56559150
Sylvain Takerkart <Sylvain.Takerkart@univ-amu.fr>
2020-05-08 07:51:09 +0200

    Changed formatting and text of the errors

--- checker/AnDO_Error.py
+++ checker/AnDO_Error.py
@@ -6,72 +6,45 @@ class ExperimentError(Exception):
 
     def __init__(self, arg):
         names = arg
-        self.strerror = '\nError1 type experiment folder error] at : ' \
-            + names[0]\
-            + '''
-            
-             ''' \
-            + '- Does not respect the naming convention for a subject directory, which is (exp-NAME) Check for structural/naming issues'
-
+        self.strerror = 'Level 1 error [experiment folder] at : ' + names[0] + '\n' \
+            + '  It should follow the exp-NAME format, where:\n' \
+            + '    - NAME is a string designating the name of your experiment\n'
 
 class SubError(Exception):
 
     def __init__(self, arg):
         names = arg
-        self.strerror = '\nError 2 type [Subject folder error] at : ' \
-            + names[1] \
-            + '''
-            
-             ''' \
-            + '- Does not respect the naming convention for a subject directory, which is (sub-GUID) Check for structural/naming issues'
-
+        self.strerror = 'Level 2 error [subject folder] at : ' + names[1] + '\n' \
+            + '  It should follow the sub-ID format, where:\n' \
+            + '    - ID is a string designating the IDentifier of the animal\n'
 
 class SessionError(Exception):
 
     def __init__(self, arg):
         names = arg
-        self.strerror = '\nError 3 type [Date folder error] at : ' \
-            + names[2] + '\n' \
-            + '\nIt does not respect the naming convention for a session directory, which is (yymmdd_xxx_a_bbbb_cccc,) with : ' \
-            + '\n' '''
-            ''' \
-            + '- yymmdd is the date in the format year, month, date (6 digits, for instance 200430 for April 30, 2020)' \
-            + '''
-            ''' \
-            + '- xxx is the number of the session acquired on that date (3 digits, for instance 001 for the first session)' \
-            + '''
-            ''' \
-            + '- a is a single letter that designate the species (m for ???, o for ???, r for ???, s for ???)' \
-            + '''
-            ''' \
-            + '- bbbb is a string containing the user friendly identifier (UFID) of the animal' \
-            + '''
-            ''' \
-            + '- cccc is a string of which the use is to be decided by the research group / user ' \
-            + '''
-            ''' \
-            + '(for instance to add extra info on the version of the experimental protocol, on the type of preparation etc.)' \
-            + '\n'
+        self.strerror = 'Level 3 error [session folder] at : ' + names[2] + '\n' \
+            + '  It should follow the YYMMDD_XXX_A_UFID_BBBB format, where:\n' \
+            + '    - YYMMDD is the date of the session (6 digits, for instance 200430 for April 30, 2020\n' \
+            + '    - XXX is the number of the session acquired on that date (3 digits, for instance 001 for the first session)\n' \
+            + '    - A is a single letter that designate the species of the animal (m for ???, o for ???, r for ???, s for ???)\n' \
+            + '    - UFID is a string containing the User Friendly IDentifier of the animal\n' \
+            + '    - BBBB is a string freely usable by the research group / user\n' \
+            + '      (for instance to add extra info on the version of the experimental protocol, on the type of preparation etc.)\n'
 
 
 class SourceError(Exception):
 
     def __init__(self, arg):
         names = arg
-        self.strerror = '\nError 4 type [Source folder error] at : ' \
-            + names[3] \
-            +'''
-            
-             ''' \
-            + '- Does not respect the naming convention for a subject directory, which is (source) Check for structural/naming issues'
-
+        self.strerror = 'Level 4 error [source folder] at : ' + names[3] + '\n' \
+            + '  A single folder called source is authorized within a session folder\n'
 
 class SourceNotFound(Exception):
 
     def __init__(self, arg):
         names = arg
-        self.strerror = \
-            '\nError 5 type [Source folder error] at : Folder source not found'
+        self.strerror = 'Level 4 error [source folder missing]\n' \
+            + '  A folder called source should be present in the session folder ' + names[2] + '\n'
 
 
 
