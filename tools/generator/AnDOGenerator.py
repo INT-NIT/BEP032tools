68354c1c479349f574d904ea8ad911c09a0c8665
Julia Sprenger <julia.sprenger@rwth-aachen.de>
2020-12-08 14:01:00 +0100

    Reformatting and typo correction

--- tools/generator/AnDOGenerator.py
+++ tools/generator/AnDOGenerator.py
@@ -25,25 +25,23 @@ def generate_Struct(csv_file, pathToDir):
     dirnames = []
     df = pd.read_csv(csv_file)
 
-    # Formating   months and  days , cannot format years
+    # Formatting months and day, cannot format years
     df["months"] = df.months.map("{:02}".format)
     df["days"] = df.days.map("{:02}".format)
-
 
     header = df.columns.values.tolist()
     # Check is the header contains the right names
     if header != COLUMNS:
         print("Failed : Csv does not have the expected columns please " \
-            + " check the documentation at 'https://github.com/INT-NIT/AnDOChecker/tools/' ")
+              + " check the documentation at 'https://github.com/INT-NIT/AnDOChecker/tools/' ")
         exit(1)
     if df.isnull().values.any():
         number_of_null_values = df.isnull().sum().sum()
-        print("There are " +str(number_of_null_values)+" null values in the cvs file")
+        print("There are " + str(number_of_null_values) + " null values in the cvs file")
         exit(1)
     list_of_information = list()
 
     for index, row in df.iterrows():
-
         my_list = [
             row["experiments_name"], row["subjects_names"],
             row["years"], row["months"], row["days"],
@@ -53,29 +51,35 @@ def generate_Struct(csv_file, pathToDir):
 
     for _, information in enumerate(list_of_information):
 
-        # Check if digits or not and addapt the string
+        # Check if digits or not and adapt the string
         if information[5] < 10:
-            num_sessions = "_00"+str(information[5])
+            num_sessions = "_00" + str(information[5])
         else:
-            num_sessions = "_0"+str(information[5])
-        
-        # Check if years format is correct 
-        if  len(str(information[2])) < 4 :
-            print("Error date format not valid at row "+ str(_))
+            num_sessions = "_0" + str(information[5])
+
+        # Check if years format is correct
+        if len(str(information[2])) < 4:
+            print("Error date format not valid at row " + str(_))
             exit(1)
 
-        dirnames.append("exp-"+str(information[0])+"/"+"sub-"+str(information[1])+"/"+'ses-'+str(
-            information[2])+str(information[3])+str(information[4])+num_sessions+"_"+str(information[6])+"/derivatives")
-        dirnames.append("exp-"+str(information[0])+"/"+"sub-"+str(information[1])+"/"+'ses-'+str(
-            information[2])+str(information[3])+str(information[4])+num_sessions+"_"+str(information[6])+"/metadata")
-        dirnames.append("exp-"+str(information[0])+"/"+"sub-"+str(information[1])+"/"+'ses-'+str(
-            information[2])+str(information[3])+str(information[4])+num_sessions+"_"+str(information[6])+"/rawdata")
+        dirnames.append(
+            "exp-" + str(information[0]) + "/" + "sub-" + str(information[1]) + "/" + 'ses-' + str(
+                information[2]) + str(information[3]) + str(
+                information[4]) + num_sessions + "_" + str(information[6]) + "/derivatives")
+        dirnames.append(
+            "exp-" + str(information[0]) + "/" + "sub-" + str(information[1]) + "/" + 'ses-' + str(
+                information[2]) + str(information[3]) + str(
+                information[4]) + num_sessions + "_" + str(information[6]) + "/metadata")
+        dirnames.append(
+            "exp-" + str(information[0]) + "/" + "sub-" + str(information[1]) + "/" + 'ses-' + str(
+                information[2]) + str(information[3]) + str(
+                information[4]) + num_sessions + "_" + str(information[6]) + "/rawdata")
 
     for directory in dirnames:
 
         try:
             # Create the directories is they do not exist
-            os.makedirs(pathToDir+'/'+str(directory))
+            os.makedirs(pathToDir + '/' + str(directory))
         except OSError:
             # Error handling when directory already exists
             print("Creation of the directory %s failed, already exist" % directory)
@@ -98,20 +102,17 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('pathToCsv', help='Path to your folder')
     parser.add_argument('pathToDir', help='Path to your csv file')
-   
+
     # Create two argument groups
-    
+
     args = parser.parse_args()
 
-  
-         # Check if directory exists
+    # Check if directory exists
     if not os.path.isdir(args.pathToDir):
-            print('Directory does not exist:', args.pathToDir)
-            exit(1)
+        print('Directory does not exist:', args.pathToDir)
+        exit(1)
     generate_Struct(args.pathToCsv, args.pathToDir)
-    
 
 
 if __name__ == '__main__':
-
     main()
