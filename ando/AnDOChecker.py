a0145580dcbe0bf8e081a6dc8cb0a65bce3b0dbb
slowblitz <jeremygarcia937@gmail.com>
2021-03-02 15:34:21 +0100

    Updated rulesStructured.py , update of the core script in AnDOChecker.py to take in count list of extension.

--- ando/AnDOChecker.py
+++ ando/AnDOChecker.py
@@ -3,11 +3,10 @@ import os.path as op
 import re
 import argparse
 
-from rulesStructured import *
+from AnDO.ando import rulesStructured as Rs
 
 
-
-def newchecker(input_directory):
+def is_valid(input_directory):
     """ Checks the ephys-BIDS validity of a data set.
 
     The specifications that define what is checked by this function is available in the following document:
@@ -15,7 +14,7 @@ def newchecker(input_directory):
 
     Parameters
     ----------
-        directory : string
+        input_directory : string
             Name of the root directory containing the data set to be checked
 
     Returns
@@ -25,8 +24,8 @@ def newchecker(input_directory):
 
         error_list : list
             List of errors (empty if the data set is valid)
-    """
 
+    """
 
     error_list = []
 
@@ -45,12 +44,26 @@ def newchecker(input_directory):
         ###
         # extract rules for this level!
         ###
-        currentdepth_rules = rules_set[depth]
+        currentdepth_rules = Rs.rules_set[depth]
         ###
         # 1. check whether the rules are followed for the folder at this level ("authorized folders")
+        #
         ###
+        if len(currentdepth_rules['mandatory_folders']) > 0:
+            # loop over rules, each rule corresponding to one mandatory folder
+            for current_mandatoryfolder_rule in (currentdepth_rules['mandatory_folders']):
+                # create the list of regexp
+                list_of_mandatory_folders = (
+                    get_list_of_rules_with(current_mandatoryfolder_rule))
+                for each_file in list_of_mandatory_folders:
+                    dir_res = [re.compile(each_file).search(d) is not None for d in dirs]
+                    if not any(dir_res):
+                        error_list.append(
+                            "Mandatory folder not found for this rule : {}".format(current_mandatoryfolder_rule))
         folder = op.split(root)[1]
-        folder_res = [re.compile(x).search(folder) is None for x in currentdepth_rules['authorized_folders']]
+        folder_res = [re.compile(x).search(folder) is None
+                      for x in get_list_of_rules_with(currentdepth_rules['authorized_folders'])]
+
         # if none of the authorized rules is respected, raise an error
         if all(folder_res):
             error_list.append("Naming rule not respected for this directory : {}".format(root))
@@ -58,10 +71,17 @@ def newchecker(input_directory):
         # 2. check whether rules are followed for files within the folder at this level ("authorized files")
         ###
         for current_file in files:
-            #### ADD generation of regular expressions based on base names and extensions
-            file_res = [re.compile(x).search(current_file) is None for x in currentdepth_rules['authorized_files']]
-            # if none of the authorized rules is respected, raise an error
-            if all(file_res):
+            file_res = list()
+            for rules in currentdepth_rules['authorized_metadata_files']:
+                file_res.extend(([re.compile(x).search(current_file) is not None for x in
+                                  get_list_of_rules_with(rules, Rs.AUTHORIZED_METADATA_EXTENSIONS)]))
+
+            for rules in currentdepth_rules['authorized_data_files']:
+                file_res.extend([re.compile(x).search(current_file) is not None for x in
+                                 get_list_of_rules_with(rules, Rs.AUTHORIZED_DATA_EXTENSIONS)])
+                # if none of the authorized rules is respected, raise an error
+
+            if not any(file_res):
                 error_list.append("Naming rule not respected for this file : {}".format(current_file))
         ###
         # 3. check whether the "mandatory files" are actually present at this level!
@@ -69,11 +89,15 @@ def newchecker(input_directory):
         if len(currentdepth_rules['mandatory_files']) > 0:
             #### ADD generation of regular expressions based on base names and extensions
             # loop over rules, each rule corresponding to one mandatory file
-            for current_mandatoryfile_rule in currentdepth_rules['mandatory_files']:
-                file_res = [re.compile(current_mandatoryfile_rule).search(f) is None for f in files]
-                # if this mandatory file is missing, raise an error
-                if all(file_res):
-                    error_list.append("Mandatory file not found for this rule : {}".format(current_mandatoryfile_rule))
+            for current_mandatoryfolder_rule in (currentdepth_rules['mandatory_files']):
+                    list_of_mandatory_folders = (get_list_of_rules_with
+                                               (current_mandatoryfolder_rule, current_mandatoryfolder_rule[1]))
+                    for each_file in list_of_mandatory_folders:
+                        file_res = [re.compile(each_file).search(f) is not None for f in files]
+
+                        if any(file_res) == False:
+                            error_list.append("Mandatory file not found for this rule : {}".
+                                              format(current_mandatoryfolder_rule))
 
     # if there are no errors, the data set is valid!
     valid = len(error_list) == 0
@@ -81,9 +105,60 @@ def newchecker(input_directory):
     return valid, error_list
 
 
+def get_list_of_rules_with(file_name_regexp, file_ext_regexp=None):
+    """
+    Function that create rules base on the regexp in rulesStructured.py
+
+    Parameters
+    ----------
+
+        file_name_regexp: list
+            list of filenames
+        file_ext_regexp: list
+            list of extensions
+
+    Returns
+    -------
+        list_of_rules : list
+            Concatenate every extension with is respective file
+
+    Examples
+    -------
+    if :
+    file_name_regexp = [
+        ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys'],
+        ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_channels'],
+        ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_contacts'],
+        ]
+
+    and :
+    file_ext_regexp = ['.tsv', '.json']
+
+    then :
+    list_of_rules = [
+        'sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys.tsv',
+        'sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys.json',
+        'sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_channels.tsv',
+        'sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_channels.json',
+        ]
+    """
+    list_of_rules = list()
+    if file_ext_regexp is None:
+        list_of_rules = file_name_regexp
+    else:
+        if len(file_name_regexp) == 2:
+            for filename in file_name_regexp[:-1]:
+                for extension in file_ext_regexp:
+                    list_of_rules.append(filename + str(extension))
+        else:
+            for filename in file_name_regexp:
+                for extension in file_ext_regexp:
+                    list_of_rules.append(filename + str(extension))
+
+    return list_of_rules
+
 
 def main():
-
     """
     Main file of the validator. uses other class methods for checking
     different aspects of the directory path.
@@ -114,8 +189,8 @@ def main():
     if not directory:
         print('Directory does not exist:', directory)
         exit(1)
-    dataset_validity, error_list = newchecker(directory)
-    if dataset_validity :
+    dataset_validity, error_list = is_valid(directory)
+    if dataset_validity:
         print("Congratulations!\n" +
               directory +
               " respects the ephys-BIDS specifications")
@@ -130,5 +205,4 @@ def main():
 
 
 if __name__ == '__main__':
-
     main()
--- ando/rulesStructured.py
+++ ando/rulesStructured.py
@@ -5,35 +5,39 @@ AUTHORIZED_METADATA_EXTENSIONS = ['.tsv', '.json']
 # level 0
 currentdepth_rules={}
 currentdepth_rules['authorized_folders'] = ['exp-([a-zA-Z0-9]+)']
+currentdepth_rules['mandatory_folders'] = [['sub-([a-zA-Z0-9]+)']]
 currentdepth_rules['authorized_data_files'] = []
 currentdepth_rules['authorized_metadata_files'] = [
-    ['subjects', ['.tsv']],
-    ['dataset_descriptions', ['.json']],
-    ['tasks', ['.tsv','.json']] # To be discussed with respect to BEP
+    ['participants'],
+    ['dataset_descriptions'],
+    ['tasks'] # To be discussed with respect to BEP
     ]
 currentdepth_rules['mandatory_files'] = [
-    ['subjects', ['.tsv']],
+    ['participants', ['.tsv']],
     ['dataset_descriptions', ['.json']]
     ]
 rules_set.append(currentdepth_rules)
 # level 1
 currentdepth_rules={}
 currentdepth_rules['authorized_folders'] = ['sub-([a-zA-Z0-9]+)']
-currentdepth_rules['authorized_data_files'] = []
+currentdepth_rules['mandatory_folders'] = [['ses-([a-zA-Z0-9]+)']]
+currentdepth_rules['authorized_data_files'] = [['^$']]
 currentdepth_rules['authorized_metadata_files'] = ['sessions']
 currentdepth_rules['mandatory_files'] = []
 rules_set.append(currentdepth_rules)
 # level 2
 currentdepth_rules={}
 currentdepth_rules['authorized_folders'] = ['ses-([a-zA-Z0-9]+)']
-currentdepth_rules['authorized_data_files'] = []
-currentdepth_rules['authorized_metadata_files'] = []
+currentdepth_rules['mandatory_folders'] = [['ephys']]
+currentdepth_rules['authorized_data_files'] = [['^$']]
+currentdepth_rules['authorized_metadata_files'] = [['^$']]
 currentdepth_rules['mandatory_files'] = []
 rules_set.append(currentdepth_rules)
 # level 3
 currentdepth_rules={}
 currentdepth_rules['authorized_folders'] = ['ephys']
-currentdepth_rules['authorized_data_files'] = ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys']
+currentdepth_rules['mandatory_folders'] = []
+currentdepth_rules['authorized_data_files'] = [['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys']]
 currentdepth_rules['authorized_metadata_files'] = [
     ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys'],
     ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_channels'],
@@ -42,7 +46,7 @@ currentdepth_rules['authorized_metadata_files'] = [
     ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_runs']
     ]
 currentdepth_rules['mandatory_files'] = [
-    ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys', ['.nwb','.nix']],
+    ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys', [f'({AUTHORIZED_DATA_EXTENSIONS[0]}|{AUTHORIZED_DATA_EXTENSIONS[1]})']],
     ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys',['.json']],
     ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_channels',['.tsv']],
     ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_contacts',['.tsv']],
