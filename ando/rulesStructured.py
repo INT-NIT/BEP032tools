0e79999b36656012376d597b7ee7c1898a99bd23
sprenger <julia.sprenger@univ-amu.fr>
2021-03-05 14:43:29 +0100

    restructure rules and fix dataset_description typo in rules

--- ando/rulesStructured.py
+++ ando/rulesStructured.py
@@ -1,20 +1,23 @@
 # construct the set of rules to be applied, level by level
 rules_set = []
-AUTHORIZED_DATA_EXTENSIONS = [f'({"|".join([".nix",".nwb"])})']
-AUTHORIZED_METADATA_EXTENSIONS = [f'({"|".join([".tsv",".json"])})']
+DATA_EXTENSIONS = [".nix", ".nwb"]
+METADATA_EXTENSIONS = [".tsv", ".json"]
+ANY_DATA_EXT = [f'({"|".join(DATA_EXTENSIONS)})']
+ANY_METADATA_EXT = [f'({"|".join(METADATA_EXTENSIONS)})']
+
 # level 0
 currentdepth_rules={}
 currentdepth_rules['authorized_folders'] = ['exp-([a-zA-Z0-9]+)']
 currentdepth_rules['mandatory_folders'] = [['sub-([a-zA-Z0-9]+)']]
 currentdepth_rules['authorized_data_files'] = []
 currentdepth_rules['authorized_metadata_files'] = [
-    ['participants',AUTHORIZED_METADATA_EXTENSIONS],
-    ['dataset_descriptions',AUTHORIZED_METADATA_EXTENSIONS],
-    ['tasks',AUTHORIZED_METADATA_EXTENSIONS] # To be discussed with respect to BEP
+    ['participants', ANY_METADATA_EXT],
+    ['dataset_description', ANY_METADATA_EXT],
+    ['tasks', ANY_METADATA_EXT]  # To be discussed with respect to BEP
     ]
 currentdepth_rules['mandatory_files'] = [
     ['participants', ['.tsv']],
-    ['dataset_descriptions', ['.json']]
+    ['dataset_description', ['.json']]
     ]
 rules_set.append(currentdepth_rules)
 # level 1
@@ -22,7 +25,7 @@ currentdepth_rules={}
 currentdepth_rules['authorized_folders'] = ['sub-([a-zA-Z0-9]+)']
 currentdepth_rules['mandatory_folders'] = [['ses-([a-zA-Z0-9]+)']]
 currentdepth_rules['authorized_data_files'] = [['^$']]
-currentdepth_rules['authorized_metadata_files'] = ['sessions']
+currentdepth_rules['authorized_metadata_files'] = [['sessions', ANY_METADATA_EXT]]
 currentdepth_rules['mandatory_files'] = []
 rules_set.append(currentdepth_rules)
 # level 2
@@ -37,16 +40,16 @@ rules_set.append(currentdepth_rules)
 currentdepth_rules={}
 currentdepth_rules['authorized_folders'] = ['ephys']
 currentdepth_rules['mandatory_folders'] = []
-currentdepth_rules['authorized_data_files'] = [['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys',AUTHORIZED_DATA_EXTENSIONS]]
+currentdepth_rules['authorized_data_files'] = [['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys', ANY_DATA_EXT]]
 currentdepth_rules['authorized_metadata_files'] = [
-    ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys',AUTHORIZED_METADATA_EXTENSIONS],
-    ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_channels',AUTHORIZED_METADATA_EXTENSIONS],
-    ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_contacts',AUTHORIZED_METADATA_EXTENSIONS],
-    ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_probes',AUTHORIZED_METADATA_EXTENSIONS],
-    ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_runs',AUTHORIZED_METADATA_EXTENSIONS]
+    ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys', ANY_METADATA_EXT],
+    ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_channels', ANY_METADATA_EXT],
+    ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_contacts', ANY_METADATA_EXT],
+    ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_probes', ANY_METADATA_EXT],
+    ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_runs', ANY_METADATA_EXT]
     ]
 currentdepth_rules['mandatory_files'] = [
-    ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys', AUTHORIZED_METADATA_EXTENSIONS],
+    ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys', ANY_METADATA_EXT],
     ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys',['.json']],
     ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_channels',['.tsv']],
     ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_contacts',['.tsv']],
