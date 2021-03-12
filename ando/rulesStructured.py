fe5bee82f5001870ddf7d5926c7028519c8e1526
sprenger <julia.sprenger@univ-amu.fr>
2021-03-12 16:13:40 +0100

    Harmonize levels of authorized and mandatory folder and remove depr 'exp-' reference

--- ando/rulesStructured.py
+++ ando/rulesStructured.py
@@ -7,7 +7,7 @@ ANY_METADATA_EXT = [f'({"|".join(METADATA_EXTENSIONS)})']
 
 # level 0
 currentdepth_rules={}
-currentdepth_rules['authorized_folders'] = ['exp-([a-zA-Z0-9]+)']
+currentdepth_rules['authorized_folders'] = ['sub-([a-zA-Z0-9]+)']
 currentdepth_rules['mandatory_folders'] = [['sub-([a-zA-Z0-9]+)']]
 currentdepth_rules['authorized_data_files'] = []
 currentdepth_rules['authorized_metadata_files'] = [
@@ -22,7 +22,7 @@ currentdepth_rules['mandatory_files'] = [
 RULES_SET.append(currentdepth_rules)
 # level 1
 currentdepth_rules={}
-currentdepth_rules['authorized_folders'] = ['sub-([a-zA-Z0-9]+)']
+currentdepth_rules['authorized_folders'] = ['ses-([a-zA-Z0-9]+)']
 currentdepth_rules['mandatory_folders'] = [['ses-([a-zA-Z0-9]+)']]
 currentdepth_rules['authorized_data_files'] = [['^$']]
 currentdepth_rules['authorized_metadata_files'] = [['sessions', ANY_METADATA_EXT]]
@@ -30,7 +30,7 @@ currentdepth_rules['mandatory_files'] = []
 RULES_SET.append(currentdepth_rules)
 # level 2
 currentdepth_rules={}
-currentdepth_rules['authorized_folders'] = ['ses-([a-zA-Z0-9]+)']
+currentdepth_rules['authorized_folders'] = ['ephys']
 currentdepth_rules['mandatory_folders'] = [['ephys']]
 currentdepth_rules['authorized_data_files'] = [['^$']]
 currentdepth_rules['authorized_metadata_files'] = [['^$']]
@@ -38,7 +38,7 @@ currentdepth_rules['mandatory_files'] = []
 RULES_SET.append(currentdepth_rules)
 # level 3
 currentdepth_rules={}
-currentdepth_rules['authorized_folders'] = ['ephys']
+currentdepth_rules['authorized_folders'] = ['^$']
 currentdepth_rules['mandatory_folders'] = []
 currentdepth_rules['authorized_data_files'] = [
     ['sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)([\\w\\-]*)_ephys', ANY_DATA_EXT]]
