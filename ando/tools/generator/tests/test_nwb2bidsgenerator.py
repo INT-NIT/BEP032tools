3d8c6d87062c814bc6bf07816a5c60eb012fef41
Saksham Sharda <saksham20.sharda@gmail.com>
2021-04-01 20:05:51 +0530

    create tests

---
+++ ando/tools/generator/tests/test_nwb2bidsgenerator.py
@@ -0,0 +1,29 @@
+import tempfile
+from pathlib import Path
+
+from datalad.api import install, Dataset
+
+from ando.tools.generator.nwb2bidsgenerator import bep_organize, is_valid
+
+
+class TestNwbBIDSGenerator:
+    def setUp(self):
+        pt = Path.cwd()/"BEP032-examples"
+        if pt.exists():
+            self.dataset = Dataset(pt)
+            self.dataset.update(merge=True)
+            self.dataset.get()
+        else:
+            self.dataset = install(
+                source="https://gin.g-node.org/NeuralEnsemble/BEP032-examples",
+                get_data=True,
+            )
+        self.datadir = pt
+        self.savedir = Path(tempfile.mkdtemp())
+
+    def test_nwb_to_bids(self):
+        bep_organize(self.datadir, output_path=self.savedir, move_nwb=True, validate=False)
+        try:
+            is_valid(self.savedir)
+        except:
+            raise Exception('Validation Error')
