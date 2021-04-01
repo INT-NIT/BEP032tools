15322935d4087d3cc7b71657e5cecd7b941b2882
sprenger <julia.sprenger@univ-amu.fr>
2021-04-01 17:54:59 +0200

    Fix CI setup (i)

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     author="Jeremy Garcia, Sylvain Takerkart",
     description="Checks the validity of a directory with respect to the ANimal Data Organization (ANDO) specifications ",
     license='MIT',
-    install_requires=['wheel', 'h5py', 'pynwb'],
+    install_requires=['pynwb'],
     include_package_data=True,
     entry_points={
 
