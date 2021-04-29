142fa69c1d06304d2fc49fa38ed1d474bd7a039a
jeremy garcia <jeremygarcia937@gmail.com>
2021-04-29 10:00:32 +0200

    Update ando/tools/generator/utils.py
    Co-authored-by: Julia Sprenger <julia.sprenger@rwth-aachen.de>

--- ando/tools/generator/utils.py
+++ ando/tools/generator/utils.py
@@ -19,7 +19,6 @@ def save_tsv(dataframe, path_to_save):
 
     """
     if path.exists(path_to_save):
-
         df = pd.read_csv(path_to_save, sep='\t')
         output = df.append(dataframe, sort=True)
         output.to_csv(path_to_save, sep="\t", index=False)
