55ff7c8daa1d76c3d7cf706bdc90e3ca1b49a686
Remi Gau <remi_gau@hotmail.com>
2021-12-07 23:02:54 +0100

    add functions to create names with pybids

--- setup.py
+++ setup.py
@@ -15,22 +15,24 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={
-            # If any package contains *.json or *.csv files, include them:
-            "": ["*.json", '*.csv', '*.tsv'],
+        # If any package contains *.json or *.csv files, include them:
+        "": ["*.json", "*.csv", "*.tsv"],
     },
     author="Jeremy Garcia, Sylvain Takerkart , Julia Sprenger",
     description="Checks the validity of a directory with respect to the ANimal Data Organization (ANDO) specifications ",
-    license='MIT',
+    license="MIT",
     install_requires=[],
     entry_points={
-        'console_scripts': ['AnDOChecker=ando.AnDOChecker:main',
-                            'AnDOGenerator=ando.tools.generator.AnDOGenerator:main',
-                            'BEP032Templater=ando.tools.generator.BEP032Templater:main',
-                            'AnDOViewer=ando.tools.viewer.AnDOViewer:main'],
+        "console_scripts": [
+            "AnDOChecker=ando.AnDOChecker:main",
+            "AnDOGenerator=ando.tools.generator.AnDOGenerator:main",
+            "BEP032Templater=ando.tools.generator.BEP032Templater:main",
+            "AnDOViewer=ando.tools.viewer.AnDOViewer:main",
+        ],
     },
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     extras_require={
-        'tools': ['pandas', 'pynwb'],
-        'test': ['pytest', 'datalad']
-    }
+        "tools": ["pandas", "pynwb", "pybids"],
+        "test": ["pytest", "datalad"],
+    },
 )
