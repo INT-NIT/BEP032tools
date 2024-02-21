143b96460e992b5c7ce7781cac859938495c9b94
fatai <sifaoufatai@gmail.com>
2024-02-21 18:31:04 +0100

    commit des test des deux fonctions  creat_directory_by_experiments, et creatfileby_directory

--- bep032/BEP032.py
+++ bep032/BEP032.py
@@ -42,11 +42,11 @@ class BEP032:
     def create_files_in_directory(self, output_path):
         subject_dir = output_path / f'sub_{self.sub_id}'
         self.create_directory_structure_by_experience(output_path)
-        """creat file within subject_dir and ephys_dir"""
+        "creat file within subject_dir and ephys_dir"
 
-        file_path_json  = os.path.join(subject_dir, f'sub_{self.sub_id}_sessions.json')
-        file_path_tsv  = os.path.join(subject_dir, f'sub_{self.sub_id}_sessions.tsv')
-        with open (file_path_json, 'w'):
+        file_path_json = os.path.join(subject_dir, f'sub_{self.sub_id}_sessions.json')
+        file_path_tsv = os.path.join(subject_dir, f'sub_{self.sub_id}_sessions.tsv')
+        with open(file_path_json, 'w'):
             pass
         with open(file_path_tsv, 'w'):
             pass
@@ -55,23 +55,11 @@ class BEP032:
         if os.path.exists(session_dir / self.modality):
             modality_dir = session_dir / self.modality
             for task in self.tasks:
-                file_path_tsv= os.path.join(modality_dir,f'sub_{self.sub_id}_ses_{self.sess_id}_task_{task}.tsv')
+                file_path_tsv = os.path.join(modality_dir,
+                                             f'sub_{self.sub_id}_ses_{self.sess_id}_task_{task}.tsv')
                 with open(file_path_tsv, 'w'):
                     pass
 
 
-
-
-
-    @staticmethod
-    def test():
-        sub_id = 'fata1'
-        sess_id = '1'
-        modality = 'ephys'
-        output_path = Path('/home/pourtoi/Bureau/Nouveau dossier/BEP')
-        bep032_instance = BEP032(sub_id, sess_id, modality, tasks='1')
-        bep032_instance.create_files_in_directory(output_path)
-
-
 if __name__ == '__main__':
-    BEP032.test()
+    pass
